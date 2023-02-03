15d2073c4e02b549820b79f6497bb0661dbf7b58
Nicolas Osorio <rnicolas.yo@gmail.com>
2023-02-02 23:26:17 -0300

    Change Makefile. Add reference to server.sh

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-build: 
-	docker build -t "sbws" .
-
 sh: 
 	bash server.sh
 
+build: 
+	docker build -t "sbws" .
+
 run:
-	docker run -d --rm -p 3000:3000 --name simple-bash-webserver sbws &
+	docker run -d --rm -p 3000:3000 --name simple-bash-webserver sbws
 
 rm:
 	docker rm -f simple-bash-webserver
--- server.sh
+++ server.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# simple bash webserver
+# ref: https://dev.to/leandronsp/building-a-web-server-in-bash-part-i-sockets-2n8b
+
 msg=${1:-"Hello World"}
 
-response="HTTP/1.1 200\r\n"
-response+="Content-Type: text/html\r\n\r\n"
-response+="${msg}"
+response='HTTP/1.1 200\r\n'
+response+='Content-Type: text/html\r\n\r\n'
+response+="<h1>${msg}<h1\>"
 
-while 1
-do
+while true; do
     echo -e "${response}" | nc -lvN 3000
 done
