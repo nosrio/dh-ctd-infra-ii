aa7f57e3a5176513ed6320bdb0e14d50f4a5f126
Nicolas Osorio <rnicolas.yo@gmail.com>
2023-03-22 22:06:02 -0300

    Agrego clase-16

---
+++ clase-16/test.js
@@ -0,0 +1,12 @@
+describe('Our application', function () {
+
+    it('This is a simple unit test', function (done) {
+
+        if (5 == 5) {
+            done();
+        } else {
+            done(new Error("Not sure what's happened."));
+        }
+    });
+
+});
